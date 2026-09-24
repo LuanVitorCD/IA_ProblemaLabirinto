1795d6b0ea1164f1475debe81e80e80cad4343dd
LuanVitorCD <luanvitorcd@gmail.com>
2026-09-24 15:16:19 -0300

    feat: add custom color to metric delta

--- main.py
+++ main.py
@@ -404,6 +404,9 @@ def main():
         div[data-testid="stMetricValue"]{
             font-size: 20px;
         }
+        div[data-testid="stMetricDelta"]{
+            color: #4f924f;
+        }
         </style>
         """,
         unsafe_allow_html=True,
